5454ef6f183fac3271be87b225b52b61c02def15
kongxiangyue <513001363@qq.com>
2018-08-30 15:14:13 +0800

    [A] 菜单案例

---
+++ code/part02/_065_menu/src/main/java/menu061/Menu061.java
@@ -0,0 +1,72 @@
+package menu061;
+
+import gen.menu061.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Menu061 extends Activity {
+	public static final int ITEM0 = Menu.FIRST;
+	public static final int ITEM1 = Menu.FIRST + 1;
+
+	
+	Button button1;
+	Button button2;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        button1 = (Button) findViewById(R.id.button1);
+		button2 = (Button) findViewById(R.id.button2);
+		button1.setVisibility(View.INVISIBLE);
+		button2.setVisibility(View.INVISIBLE);
+		
+
+    }
+	@Override
+	/*
+	 * menu.findItem(EXIT_ID);找到特定的MenuItem
+	 * MenuItem.setIcon.可以设置menu按钮的背景
+	 */
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, ITEM0, 0, "此时显示button1");
+		menu.add(0, ITEM1, 0, "此时显示button2");
+		menu.findItem(ITEM1);
+		return true;
+	}
+	
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case ITEM0: 
+			actionClickMenuItem1();
+		break;
+		case ITEM1: 
+			actionClickMenuItem2(); break;
+
+		}
+		return super.onOptionsItemSelected(item);}
+	/*
+	 * 点击第一个menu的第一个按钮执行的动作
+	 */
+	private void actionClickMenuItem1(){
+		setTitle("button1 可见");
+		button1.setVisibility(View.VISIBLE);
+		button2.setVisibility(View.INVISIBLE);
+	}
+	/*
+	 * 点击第二个个menu的第一个按钮执行的动作
+	 */
+	private void actionClickMenuItem2(){
+		setTitle("可以看到button2 ");
+		button1.setVisibility(View.INVISIBLE);
+		button2.setVisibility(View.VISIBLE);
+	}
+}
